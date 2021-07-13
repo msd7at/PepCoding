2ff5bd00cb0a4f948d4581e68392b07d1e286011
Anurag Tiwari <anurag.7140@gmail.com>
2021-07-14 00:40:29 +0530

    abc

---
+++ DSA LEVEL 1/Recursion with arraylists/Get Maze Paths.py
@@ -0,0 +1,25 @@
+import sys
+sys.setrecursionlimit(10**6)
+def prn(i,j,n,m):
+    if i==n-1 and j==m-1:
+        return [""]
+    h=[]
+    v=[]
+    if i<n:
+        h= prn(i+1,j,n,m)
+    if j<m:
+        v=prn(i,j+1,n,m)
+    path=[]
+    for i in h:
+        path.append("h"+i)
+    for i in v:
+        path.append("v"+i)
+    return path
+
+
+
+if __name__ == "__main__":
+    n=int(input())
+    m=int(input())
+    q=prn(0,0,n,m)
+    print(q)
